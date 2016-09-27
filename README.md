77cba0de76dfdaf1fd1da3e300102f838fff88ac
Dylan Wilson <dylan@craftworkgames.com>
2016-09-27 22:20:17 +1000

    it compiles.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # MonoGame.Squid
-An implementation of the Squid GUI library for MonoGame
+
+An implementation of the Squid GUI library for MonoGame.
+
+SQUID is an SDK to create user interfaces for games and other 2D/3D realtime applications, 
+using a Retained Mode system. SQUID does not depend on a certain rendering engine, you can 
+use it with any engine you like, on any platform that supports the .NET 2.0 framework.
+
+It was originally created by [IONSTAR Studios](http://www.ionstar.org/?page_id=4).
+
+Squid was [made open source](http://ionstar.org/forum/index.php?PHPSESSID=ie2s5imp691rqr8t3e0oahbtp1&topic=195.0) on April 18, 2016. 
+The original source code can be found [on github](https://github.com/Roderik11/Squid).
+
+This fork adds a renderer for MonoGame.
---
+++ Source/Demo/Game1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Demo
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            var gamePadState = GamePad.GetState(PlayerIndex.One);
+            var keyboardState = Keyboard.GetState();
+
+            if (gamePadState.Buttons.Back == ButtonState.Pressed || keyboardState.IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
